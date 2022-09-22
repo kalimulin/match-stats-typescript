641f2d92a2f6a8a72dcfce37c97b38aadb01841a
Rustam Kalimulin <r.kalimulin@notamedia.ru>
2022-09-22 10:41:54 +0400

    плохой js код

--- build/index.js
+++ build/index.js
@@ -7,4 +7,14 @@ const fs_1 = __importDefault(require("fs"));
 const matches = fs_1.default.readFileSync('football.csv', {
     encoding: 'utf-8'
 }).split('\n').map(item => item.split(','));
-console.log(matches);
+// [дата, хозяева, гости, забили хозяева, забили гости, победитель (H | D | A), ]
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === 'A') {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} times.`);
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,16 @@ const matches = fs.readFileSync('football.csv', {
   encoding: 'utf-8'
 }).split('\n').map(item => item.split(','));
 
-console.log(matches);
+// [дата, хозяева, гости, забили хозяева, забили гости, победитель (H | D | A), ]
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if(match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} times.`)
