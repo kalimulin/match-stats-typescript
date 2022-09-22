a323b18972a006af4044a890a710939d7e09e022
Rustam Kalimulin <r.kalimulin@notamedia.ru>
2022-09-22 14:20:52 +0400

    Enum

--- build/index.js
+++ build/index.js
@@ -8,12 +8,21 @@ const matches = fs_1.default.readFileSync('football.csv', {
     encoding: 'utf-8'
 }).split('\n').map(item => item.split(','));
 // [дата, хозяева, гости, забили хозяева, забили гости, победитель (H | D | A), ]
+const homeWin = 'H';
+const awayWin = 'A';
+// Enum - enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
 let manUnitedWins = 0;
 for (let match of matches) {
-    if (match[1] === 'Man United' && match[5] === 'H') {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === 'A') {
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { match } from 'assert';
 import fs from 'fs';
 
 const matches = fs.readFileSync('football.csv', {
@@ -6,12 +7,22 @@ const matches = fs.readFileSync('football.csv', {
 
 // [дата, хозяева, гости, забили хозяева, забили гости, победитель (H | D | A), ]
 
+const homeWin = 'H';
+const awayWin = 'A';
+
+// Enum - enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
 let manUnitedWins = 0;
 
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if(match[2] === 'Man United' && match[5] === 'A') {
+  } else if(match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
