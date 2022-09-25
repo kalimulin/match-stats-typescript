eac20811427f0e114b7abbbcce80228e9189da86
Rustam Kalimulin <r.kalimulin@notamedia.ru>
2022-09-25 10:23:58 +0400

    статические методы

--- build/index.js
+++ build/index.js
@@ -1,15 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
-const CsvFileReader_1 = require("./CsvFileReader");
 const MatchReader_1 = require("./MatchReader");
-const ConsoleReport_1 = require("./reportTargets/ConsoleReport");
-const HtmlReport_1 = require("./reportTargets/HtmlReport");
 const Summary_1 = require("./Summary");
-const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
-const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+const matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
 matchReader.load();
-const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+const summary = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
 summary.buildAndPrintReport(matchReader.matches);
-const summaryHtml = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new HtmlReport_1.HtmlReport());
-summaryHtml.buildAndPrintReport(matchReader.matches);
