6cc08126f5fe9c89a63001770d2f0e5bfab22588
Rustam Kalimulin <r.kalimulin@notamedia.ru>
2022-09-25 09:06:36 +0400

    HTML report

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 import { Summary } from "./Summary";
 
 const csvFileReader = new CsvFileReader('football.csv');
@@ -10,3 +11,6 @@ matchReader.load();
 
 const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
 summary.buildAndPrintReport(matchReader.matches);
+
+const summaryHtml = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+summaryHtml.buildAndPrintReport(matchReader.matches);
---
+++ src/reportTargets/HtmlReport.ts
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import { OutputTarget } from "../Summary";
+
+export class HtmlReport implements OutputTarget {
+  print(report: string): void {
+    const html = `
+    <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Report</title>
+      </head>
+      <body>
+        <h1>Analysis Output</h1>
+        <div>${report}</div>
+      </body>
+      </html>
+    `
+    fs.writeFileSync('report.html', html);
+  }
+}
